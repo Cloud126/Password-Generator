7e769a809a8d24c55955a177878f18a4061288a2
Maurino Federico Hernandez <maurinofederico96@gmail.com>
2020-12-15 22:23:01 -0500

    Final Touches on README.md

--- README.md
+++ README.md
@@ -3,17 +3,20 @@ JavaScript: Password Generator
 
 ## Why I did this homework
 
-The goal of this homework is to create a web application that will generate a random password with four choices of criteria. These are lowercase, uppercase, numeric, and/or special characters. The application must also be a responsive web application and have a clean user interface.
+The goal of this homework is to create a web application that will generate a secured password with four choices of criteria. These are lowercase, uppercase, numeric, and/or special characters. The user will choose which ones they would like afterwards, generating it randomly. The application must also be a responsive web application and have a clean user interface.
 
 ## What I learned
-
-
+This homework was a very challenging one. It took everything that has been taught to the class so far, plus more. JavaScript is especially difficult for me. It reviewed a lot of the material we have covered in class, some of these are arrays and functions. Also if statements, which are heavily used in my codes to determine which of the options have been confirmed to be true using prompts and confirm interactions. Pseudo coding was very useful in determining where I needed to start and what steps to take in order to complete my goal.
 
 ## Link to Github
 
 https://github.com/Cloud126/Password-Generator
 
-## Link to LIVE URL
+## Apliciation Features
+
+When the "Generate Password" button is clicked. A prompt will appear asking the user how long they would like their password to be, and that it must be between 8 and 128 characters. After it will ask the user to confirm if they would like to include lowercase, uppercase, numeric, and/or special characters. When all is decided the users random password will appear in place of "Yours Secure Password" placeholder.
+
+## Link to Application
 
 https://cloud126.github.io/Password-Generator/
 
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var number = [0,1,2,3,4,5,6,7,8,9];
 
 function generatePassword() {
-    //User's desire length for password
+    //User's desire length for password//
     var pLength = parseInt(prompt("How long would you like your password to be? Must be between 8 and 128."));
     //This will show message if user doesn't type in value for desired password length
     if (isNaN(length) === true){
@@ -15,22 +15,22 @@ function generatePassword() {
       return;
     }
     
-    //if user types above 128
+    //if user chooses above 128//
     if(pLength > 128) {
       alert("Must be between 8 and 128 characters.");
       return;
     }
-    //if user chooses below 8
+    //if user chooses below 8//
     if(pLength < 8) {
       alert("Must be between 8 and 128 characters.");
       return;
     }
-    //confirmation for criteria options
+    //confirmation for criteria options//
     var hasSpecial = confirm("Do you want to use special characters?");
     var hasUppercase = confirm("Do you want to use uppercase?");
     var hasLowercase = confirm("Do you want to use lowercase?");
     var hasNumber = confirm("Do you want to use numbers?");
-    //if user does not choose any criteria
+    //if user does not choose any criteria//
     if(hasSpecial === false &&
       hasUppercase === false &&
       hasLowercase === false &&
@@ -39,11 +39,11 @@ function generatePassword() {
       return;
     }
 
-    //If and else statements for options
+    //If and else statements for options//
     if(hasSpecial === true && hasUppercase === true && hasLowercase === true && hasNumber === true) {
       choices = special.concat(uppercase,lowercase,number);
     }
-    /*3 option*/
+   
     else if(hasSpecial === true && hasUppercase === true && hasLowercase === true) {
       choices = special.concat(uppercase,lowercase);
     }
@@ -59,7 +59,7 @@ function generatePassword() {
     else if(hasUppercase === true && hasLowercase === true && hasNumber === true) {
       choices = uppercase.concat(lowercase,number);
     }
-    /*2 option*/
+  
     else if(hasSpecial === true && hasUppercase === true){
       choices = special.concat(uppercase);
     }
@@ -83,7 +83,7 @@ function generatePassword() {
     else if(hasLowercase === true && hasNumber === true) {
       choices = lowercase.concat(number);
     }
-    /*1 option*/
+    
     else if(hasSpecial === true) {
       choices = special;
     }
@@ -103,8 +103,8 @@ function generatePassword() {
     var finalPassword = [];
     
     for(var i = 0; i < pLength; i++) {
-      var userPicks = choices[Math.floor(Math.random() * choices.length)];
-      finalPassword.push(userPicks);
+      var userChoices = choices[Math.floor(Math.random() * choices.length)];
+      finalPassword.push(userChoices);
     }
       return finalPassword.join("");
   }
